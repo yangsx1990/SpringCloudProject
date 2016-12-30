024cb4cc6ca84c67c62dd93a49387c9cd07dfe71
yangsaixing <yangsaixing@yfywyangsx.hiersun.com>
2016-12-30 20:12:12 +0800

    修改配置信息

--- BalanceProject/src/main/java/com/common/RibbonApplication.java
+++ BalanceProject/src/main/java/com/common/RibbonApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -14,17 +15,18 @@ import org.springframework.web.client.RestTemplate;
  * @version 1.0 
  */
 @SpringBootApplication
-@EnableDiscoveryClient
-@EnableCircuitBreaker //开启熔断器
+//@EnableDiscoveryClient
+//@EnableCircuitBreaker //开启熔断器
+@EnableConfigServer //开启配置中心
 public class RibbonApplication {
 
-		@Bean
-		@LoadBalanced   //�������⸺������
-		RestTemplate restTemplate() {
-			return new RestTemplate();
-		}
-		public static void main(String[] args) {
-			SpringApplication.run(RibbonApplication.class, args);
-		}
+	/*@Bean
+	@LoadBalanced   //�������⸺������
+	RestTemplate restTemplate() {
+		return new RestTemplate();
+	}*/
+	public static void main(String[] args) {
+		SpringApplication.run(RibbonApplication.class, args);
+	}
 	
 }
